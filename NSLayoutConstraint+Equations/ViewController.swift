70c28f201d0572141c832ad1a7aecad94f389319
Ahmed Ragab <ahmed.ragab@me.com>
2015-12-12 17:46:59 +0200

    Initial Commit

---
+++ NSLayoutConstraint+Equations/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NSLayoutConstraint+Equations
+//
+//  Created by Ahmed Ragab on 12/12/15.
+//  Copyright © 2015 QuaNode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
